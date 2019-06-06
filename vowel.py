5d0d8ec17526ee2aba021fdbffa4982258d23c0f
prabujeeva <42169857+prabujeeva@users.noreply.github.com>
2019-06-06 11:34:03 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,6 @@
+char=input("enter the character:")
+
+if(char=='a'or char=='e' or char=='i' or char=='o' or char=='u')
+    print (char,  "is a vowel" )
+else:
+    print (char,   "is a constanent")
