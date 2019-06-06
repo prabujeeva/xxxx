145da669c0de90c56ec2ae6f0ab8cf199fb4b372
prabujeeva <42169857+prabujeeva@users.noreply.github.com>
2019-06-06 12:14:59 +0530

    Update alp.py

--- alp.py
+++ alp.py
@@ -1,6 +1,6 @@
 char=input()
 
 if ((char>='A' and char<='Z') or (char>='a' and char<='z')):
-    print (char,  "is a alphabet" )
+    print (char is d,  "is a alphabet" )
 else:
     print (char,   "not")
